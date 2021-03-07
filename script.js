bccb9982474060ca648b5e25ec90bea299bd3e5d
Nam Nguyen <nguyenthanhnamjuly27th1990@gmail.com>
2021-03-07 18:15:16 +0700

    running in Parallel

--- script.js
+++ script.js
@@ -396,17 +396,42 @@ const whereAmI = async function () {
     renderError(`sth wrong $$$ ${err.message}`);
 
     // Reject promise returned from async function
-    throw err
+    throw err;
   }
 };
 
+// (async function() {
+//   try {
+//     const city= await whereAmI();
+//     console.log(`2: ${city}`);
+//   }catch(err) {
+//     console.log(`2: ${err.message} ❤`);
+//   }
+//   console.log('3: Finished getting location');
+// })();
 
-(async function() {
+const get3Countries = async function (c1, c2, c3) {
   try {
-    const city= await whereAmI();
-    console.log(`2: ${city}`);
-  }catch(err) {
-    console.log(`2: ${err.message} ❤`);
+    // const [data1] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c1}`
+    // );
+    // const [data2] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c2}`
+    // );
+    // const [data3] = await getJSON(
+    //   `https://restcountries.eu/rest/v2/name/${c3}`
+    // );
+    // console.log([data1.capital, data2.capital, data3.capital]);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.eu/rest/v2/name/${c1}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c2}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c3}`)
+     ] );
+     console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    console.log(err);
   }
-  console.log('3: Finished getting location');
-})();
+};
+
+get3Countries('USA', 'Georgia', 'Belgium');
